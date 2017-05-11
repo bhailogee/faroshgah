c8c2f778745c762d391b8fc3e975ba35b7fae1fa
mwasu <mwaseem@sequelmed.com>
2017-05-12 01:53:00 +0500

    datasources changed

--- backend/server/boot/discovery.js
+++ backend/server/boot/discovery.js
@@ -1,20 +1,39 @@
 var config =require('config');
 var Promise = require('bluebird');
+var _=require("underscore");
 
-module.exports=function(app){
+module.exports=function(app) {
 
-    if(config.datasources.discovery)
-    {
-        config.datasources.db.forEach(function(dsName) {
-            var ds = app.datasources[dsName];
-            Promise.promisifyAll(ds);
+    if (config.datasources.discovery) {
 
-            ds.discoverModelDefinitions({views: true, limit: 20},function(r){
-               var rr=r;
+        var ds = app.datasources[config.datasources.ds];
+        Promise.promisifyAll(ds);
+
+        /* ds.discoverAndBuildModels('store',{visited: {}, associations: true},function(er,models){
+
+         models.Store.findOne({},function(err,st){
+         var a=st;
+         })
+         });
+         });
+         */
+
+        ds.discoverModelDefinitions({"schema": config.datasources.db}, function (er, models) {
+
+            var models = _.map(models,function (modelName) {
+                return ds.discoverAndBuildModelsAsync(modelName.name, {
+                    "schema": config.datasources.db,
+                    "relations": true
+                });
             });
 
+            Promise.all(models).then(function(a){
+                a.forEach(function(m){
+                    //exposing these models over rest
+                    //app.model(m,{"public":true});
+                });
+            });
         });
-
     }
 
 }
